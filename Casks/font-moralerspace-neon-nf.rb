db1a2d7dacb2a0133163dc4a4ee2dcb5d4fabc18
skpersonal <50128722+skpersonal@users.noreply.github.com>
2024-02-22 18:46:11 +0900

    Add desc

--- Casks/font-moralerspace-neon-nf.rb
+++ Casks/font-moralerspace-neon-nf.rb
@@ -4,10 +4,11 @@ cask "font-moralerspace-neon-nf" do
   
     url "https://github.com/yuru7/moralerspace/releases/download/v#{version}/MoralerspaceNF_v#{version}.zip"
     name "Moralerspace Neon NF"
+    desc "Moralerspace is a programming font that combines Monaspace and IBM Plex Sans JP"
     homepage "https://github.com/yuru7/moralerspace"
   
     font "MoralerspaceNF_v#{version}/MoralerspaceNeonNF-Bold.ttf"
     font "MoralerspaceNF_v#{version}/MoralerspaceNeonNF-BoldItalic.ttf"
     font "MoralerspaceNF_v#{version}/MoralerspaceNeonNF-Italic.ttf"
     font "MoralerspaceNF_v#{version}/MoralerspaceNeonNF-Regular.ttf"
-  end
+end
