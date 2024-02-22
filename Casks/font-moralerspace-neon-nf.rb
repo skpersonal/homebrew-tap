e950a75370f813b79ef2739169d0bfad7a07f431
skpersonal <50128722+skpersonal@users.noreply.github.com>
2024-02-22 18:58:56 +0900

    Test livecheck

--- Casks/font-moralerspace-neon-nf.rb
+++ Casks/font-moralerspace-neon-nf.rb
@@ -1,5 +1,5 @@
 cask "font-moralerspace-neon-nf" do
-    version "0.0.8"
+    version :latest
     sha256 :no_check
   
     url "https://github.com/yuru7/moralerspace/releases/download/v#{version}/MoralerspaceNF_v#{version}.zip"
@@ -11,4 +11,9 @@ cask "font-moralerspace-neon-nf" do
     font "MoralerspaceNF_v#{version}/MoralerspaceNeonNF-BoldItalic.ttf"
     font "MoralerspaceNF_v#{version}/MoralerspaceNeonNF-Italic.ttf"
     font "MoralerspaceNF_v#{version}/MoralerspaceNeonNF-Regular.ttf"
+end
+
+livecheck do
+    url "https://github.com/yuru7/moralerspace.git"
+    regex(/^(\d+(?:\.\d+)+)$/i)
 end
