c80da329ebf6c806c3755b0daf40e6e716ec56fe
skpersonal <50128722+skpersonal@users.noreply.github.com>
2024-02-22 17:23:54 +0900

    Create font-moralerspace-neon-nf.rb

---
+++ font-moralerspace-neon-nf.rb
@@ -0,0 +1,13 @@
+cask "font-moralerspace-neon-nf" do
+    version "0.0.8"
+    sha256 "ad7b412fd05817c39f506ebe942b9e527504b626bae80b8cce866cd7eae9d99c"
+  
+    url "https://github.com/yuru7/moralerspace/releases/download/v#{version}/MoralerspaceNF_v#{version}.zip"
+    name "Moralerspace Neon NF"
+    homepage "https://github.com/yuru7/moralerspace"
+  
+    font "MoralerspaceNeonNF-Bold.otf"
+    font "MoralerspaceNeonNF-BoldItalic.otf"
+    font "MoralerspaceNeonNF-Italic.otf"
+    font "MoralerspaceNeonNF-Regular.otf"
+  end
