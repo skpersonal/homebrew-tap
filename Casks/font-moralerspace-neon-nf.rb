da06b5e04167decadff7df4b9484296ba0241437
skpersonal <50128722+skpersonal@users.noreply.github.com>
2024-04-09 17:37:43 +0900

    sha256の追加

--- Casks/font-moralerspace-neon-nf.rb
+++ Casks/font-moralerspace-neon-nf.rb
@@ -1,6 +1,6 @@
 cask "font-moralerspace-neon-nf" do
     version "v1.0.0"
-    sha256 :no_check
+    sha256 "728196c407a921e5e2905aedebe18ffea5370598f346b0a25a13c0bf5e02c974"
   
     url "https://github.com/yuru7/moralerspace/releases/download/#{version}/MoralerspaceNF_#{version}.zip"
     name "Moralerspace Neon NF"
--- Casks/font-source-han-code-jp.rb
+++ Casks/font-source-han-code-jp.rb
@@ -1,6 +1,6 @@
 cask "font-source-han-code-jp" do
     version "2.012R"
-    sha256 :no_check
+    sha256 "1d0438f0623be0761d5ffb105f61767bd7f2b71fbc95dafd00bee9a332f62585"
   
     url "https://github.com/adobe-fonts/source-han-code-jp/archive/refs/tags/#{version}.zip"
     name "Source Han Code JP"
