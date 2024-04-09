21c8b3b44b5003d250cb7d4a88c2f6a3aeb53659
skpersonal <50128722+skpersonal@users.noreply.github.com>
2024-04-09 13:35:54 +0900

    sha256を計算するように変更

--- Scripts/update_version.js
+++ Scripts/update_version.js
@@ -2,6 +2,8 @@
 const fs = require("fs");
 const { exit } = require("process");
 const axios = require("axios");
+const checksum = require("checksum");
+const Downloader = require("nodejs-file-downloader");
 
 const templatePath = (name) => `Templates/${name}.rb.tmp`;
 const caskPath = (name) => `Casks/${name}.rb`;
@@ -34,8 +36,39 @@ const main = () => {
       .then((res) => {
         const version = res.data.tag_name;
         const template = fs.readFileSync(templatePath(caskName)).toString();
-        const code = template.replace("{{ version }}", `"${version}"`);
-        fs.writeFileSync(caskPath(caskName), code);
+        const versionUpdated = template.replace(
+          "{{ version }}",
+          `"${version}"`
+        );
+        const dlTmp = versionUpdated.replaceAll("#{version}", version);
+        const downloadUrlRegexp = new RegExp(
+          /https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+/g
+        );
+        const downloadUrl = dlTmp.match(downloadUrlRegexp)[0];
+        (async () => {
+          const dlFile = new Downloader({
+            url: downloadUrl,
+            directory: "./downloads",
+            fileName: `${caskName}.${downloadUrl.split(".").pop()}`,
+            cloneFiles: false,
+          });
+          try {
+            const { filePath, downloadStatus } = await dlFile.download();
+            checksum.file(filePath, { algorithm: "sha256" }, (err, sha256) => {
+              if (err) {
+                console.error(`Error: ${err}`);
+                return;
+              }
+              const sha256Updated = versionUpdated.replace(
+                "{{ sha256 }}",
+                `"${sha256}"`
+              );
+              fs.writeFileSync(caskPath(caskName), sha256Updated);
+            });
+          } catch (error) {
+            console.error("Download failed", error);
+          }
+        })();
       })
       .catch((err) => {
         console.error(`Error: ${err}`);
